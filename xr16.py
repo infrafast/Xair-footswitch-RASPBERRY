b1a386ea725604a6a7dcef8644f81fa008a85230
infrafast <info@infrafast.com>
2019-12-09 15:25:38 +0100

    Revert "tapdelay /4"
    This reverts commit 017bb64ee1e5243be371f895fdd1399793324f8d.

--- xr16.py
+++ xr16.py
@@ -123,7 +123,7 @@ def asyncRxThread(portNum):
                                         
                 if (msg[0]==tapCommand):
                     #value 1.0 = 3000 ms
-                    delaytime=(msg[2]*3000)/4
+                    delaytime=(msg[2]*3000)*2
     print("asyncRxThread exit ")
 
 def allLedsOff():
@@ -412,7 +412,7 @@ device = None
 txSocket = None
 setupGPIO()
 
-print ("Xair footswitch v0.22")
+print ("Xair footswitch v0.21")
 print ("Press Ctrl+Z to exit")
 print ("")
     
